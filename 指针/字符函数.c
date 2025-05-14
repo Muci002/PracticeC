ded95268f7ee311feb62e0cfeafa026959be41ae
Muci002 <2963849040@qq.com>
2025-05-14 22:39:16 +0800

    小写转大写

---
+++ 指针/字符函数.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+	char arr[] = "is big";
+	for (int i = 0; arr[i] != '\0'; i++)
+	{
+		if (islower(arr[i]))
+		{
+			arr[i] = toupper(arr[i]);
+		}
+	}
+
+	printf("%s\n", arr);
+	return 0;
+}
