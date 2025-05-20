785e13b40b2aea934a9ee542261318e6b38b4606
Muci002 <2963849040@qq.com>
2025-05-20 14:37:51 +0800

    内存块重叠的拷贝

--- 指针/内存函数.c
+++ 指针/内存函数.c
@@ -17,6 +17,29 @@ void* MyMemCpy(void* p1, void* p2, size_t num)
 	return p1;
 }
 
+//内存重叠的拷贝
+void* MyMemMove(const void* p1, const void* p2, size_t num)
+{
+	assert(p1 && p2);
+	char* cur1 = (char*)p1;
+	char* cur2 = (char*)p2;
+	if (p2 <= p1) //后 -> 前
+	{
+		while (num--)
+		{
+			*(cur1 + num) = *(cur2 + num);
+		}
+	}
+	else
+	{
+		while (num--)
+		{
+			*cur1++ = *cur2++;
+		}
+	}
+	return p1;
+}
+
 int main()
 {
 	char arr1[] = "abcdef";
