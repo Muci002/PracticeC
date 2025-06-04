982ade2c780c5ea798cae43246d57fc363bbc568
Muci002 <2963849040@qq.com>
2025-06-04 10:53:56 +0800

    算法题

--- CPP/数据结构/链表/洛谷B3630排队顺序01.cpp
+++ CPP/数据结构/链表/洛谷B3630排队顺序01.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+const int N = 1e6+10;
+int ne[N], h;
+
 int main()
 {
-
+    int n = 0; 
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> ne[i];
+    }
+    cin >> h;
+    for(int i = h; i ; i = ne[i])
+    {
+        cout << i  << " ";
+    }
     return 0;
 }
