3b468e4ef9c6fd37f26bd103238de14c0838f352
Muci002 <2963849040@qq.com>
2025-06-21 19:29:21 +0800

    顺序表

---
+++ CPP/数据结构/1顺序表/5寄包柜.cpp
@@ -0,0 +1,36 @@
+//P3613
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+vector<int> a[N];
+int n, q;
+
+int main()
+{
+	cin >> n >> q;
+	while (q--)
+	{
+		int op = 0, i = 0, j = 0, x = 0;
+		cin >> op >> i >> j;
+		if (op == 1)
+		{
+			cin >> x;
+			if (a[i].size() < j)
+			{
+				a[i].resize(j + 1);
+			}
+			a[i][j] = x;
+		}
+		else if (op == 2)
+		{
+			cout << a[i][j] << endl;
+		}
+	}
+
+	return 0;
+}
