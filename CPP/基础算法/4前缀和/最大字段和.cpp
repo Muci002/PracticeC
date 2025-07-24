11e13013d80145915d5011b7db6e95752884cd97
Muci002 <2963849040@qq.com>
2025-07-24 15:09:47 +0800

    一维前缀和

---
+++ CPP/基础算法/4前缀和/最大字段和.cpp
@@ -0,0 +1,26 @@
+P1115
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+const int N = 2 * 1e5 + 10;
+typedef long long LL;
+LL f[N];
+LL n;
+
+int main()
+{
+	cin >> n;
+	LL minvalu = 0x3f3f3f3f;
+	LL ret = -0x3f3f3f3f;
+	for (int i = 1; i <= n; i++)
+	{
+		LL x; cin >> x;
+		f[i] = f[i - 1] + x;
+		minvalu = min(minvalu, f[i-1]);
+		ret = max(ret, f[i] - minvalu);
+	}
+	cout << ret << endl;
+	return  0;
+}
