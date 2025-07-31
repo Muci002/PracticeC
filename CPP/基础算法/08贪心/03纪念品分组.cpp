2750e84300be0647f2c6c39c283b5731179ff062
Muci002 <2963849040@qq.com>
2025-07-31 17:04:45 +0800

    贪心

---
+++ CPP/基础算法/08贪心/03纪念品分组.cpp
@@ -0,0 +1,71 @@
+// P1094
+
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+const int N = 3 * 1e4 + 10;
+int a[N], n, w;
+unordered_map<int, int> mp;
+
+int main()
+{
+	cin >> w >> n;
+	for (int i = 1; i <= n; i++) 
+	{
+		cin >> a[i];
+		mp[a[i]]++;
+	}
+	sort(a + 1, a + 1 + n);
+	int cnt = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if(mp[a[i]])
+		{
+			int tmp = w - a[i];
+			mp[a[i]]--;
+			auto t = upper_bound(a + 1, a + 1 + n, tmp);
+			while ((--t) != (a))
+			{
+				if (*t <= tmp && mp.count(*t) && mp[*t])
+				{
+					mp[*t]--;
+					break;
+				}
+			}
+			
+			cnt++;
+		}
+	}
+	cout << cnt;
+	return 0;
+}
+
+
+
+//······························································
+//······························································
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+const int N = 3 * 1e4 + 10;
+int a[N], n, w;
+
+int main()
+{
+	cin >> w >> n;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	sort(a + 1, a + n + 1);
+	int l = 1, r = n; 
+	int cnt = 0;
+	while (l <= r)
+	{
+		if (a[l] + a[r] <= w) l++; 
+		r--;
+		cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
