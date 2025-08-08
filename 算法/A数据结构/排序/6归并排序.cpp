3c2f528352af64dde6a788fb2bc69ca9ea49b157
Muci002 <2963849040@qq.com>
2025-08-08 16:02:24 +0800

    归并排序

--- 算法/A数据结构/排序/6归并排序.cpp
+++ 算法/A数据结构/排序/6归并排序.cpp
@@ -8,3 +8,54 @@
 // 其中，如何让左右两边有序，就继续交给归并排序。
 // - 因此归并排序是用递归来实现的；
 // - 合并两个有序区间的操作，在顺序表中讲过类似的算法题。
+
+
+#include <iostream>
+#include <utility>
+using namespace std;
+
+const int N = 1e5 + 10;
+int n, arr[N];
+int tmp[N];  // 辅助归并排序，合并两个有序数组
+
+void merge_sort(int left, int right)
+{
+	if (left >= right) return;
+	
+	// 1.先一分为二
+	int mid = (left + right) >> 1;
+	// [left, mid]  [mid + 1, right]
+	// 2.先让左右区间有序
+	merge_sort(left, mid);
+	merge_sort(mid + 1, right);
+
+	// 合并两个有序数组
+	int cur1 = left, cur2 = mid + 1, i = left;
+	// [left, mid]  [mid + 1, right]
+	while (cur1 <= mid && cur2 <= right)
+	{
+		if (arr[cur1] <= arr[cur2]) tmp[i++] = arr[cur1++];
+		else tmp[i++] = arr[cur2++];
+	}
+
+	while(cur1 <= mid) tmp[i++] = arr[cur1++];
+	while(cur2 <= mid) tmp[i++] = arr[cur2++];
+
+	for (int j = left; j <= right; j++)
+	{
+		arr[j] = tmp[j];
+	}
+
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++) cin >> arr[i];
+
+	merge_sort(1, n);
+
+	for (int i = 1; i <= n; i++) cout << arr[i] << " ";
+
+	return 0;
+}
