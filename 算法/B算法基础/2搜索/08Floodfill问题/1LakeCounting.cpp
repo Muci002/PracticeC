be9c219a1aac4b72bbf812691d525be48f1e1504
Muci002 <2963849040@qq.com>
2025-08-19 10:13:10 +0800

    BFS

--- 算法/B算法基础/2搜索/08Floodfill问题/1LakeCounting.cpp
+++ 算法/B算法基础/2搜索/08Floodfill问题/1LakeCounting.cpp
@@ -3,3 +3,60 @@
 // FloodFill算法，又称漫水填充，像素法
 // 本质是在寻找具有相同性质的联通块
 
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+typedef pair<int, int> PII;
+const int N = 110;
+char a[N][N];
+bool st[N][N];
+int n, m;
+int dx[] = { 0, 0, -1, 1, 1, 1, -1 , -1 };
+int dy[] = { 1, -1, 0, 0, 1, -1, -1, 1 };
+
+void dfs(int i, int j)
+{
+	st[i][j] = true;
+
+	for (int k = 0; k < 8; k++)
+	{
+		int x = i + dx[k], y = j + dy[k];
+		if (x >= 1 && x <= n && y >= 1 && y <= m && a[x][y] == 'W' && st[x][y] == false)
+		{
+			dfs(x, y);
+		}
+	} 
+
+}
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n;i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			cin >> a[i][j];
+			//cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	int ret = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			if (a[i][j] == 'W' && st[i][j] == false)
+			{
+				ret++;
+				dfs(i, j);
+			}
+
+		}
+	}
+	cout << ret;
+	return 0;
+}
