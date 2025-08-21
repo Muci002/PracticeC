f5677065f1a2631f89642dbf13a98e8be3314bbf
Muci002 <2963849040@qq.com>
2025-08-21 20:55:17 +0800

    单调队列

--- 算法/B算法基础/3数据结构/02单调队列/02质量检测.cpp
+++ 算法/B算法基础/3数据结构/02单调队列/02质量检测.cpp
@@ -1,2 +1,26 @@
 // P2251
 
+#include <iostream>
+#include <deque>
+
+using namespace std;
+const int N = 1e5 + 10;
+int a[N];
+int n, m;
+
+int main()
+{
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) cin >> a[i];
+	
+	deque<int> q; // 存下标
+	for (int i = 1; i <= n; i++)
+	{
+		while (q.size() && a[q.back()] >= a[i]) q.pop_back();
+		q.push_back(i);
+		if ((q.back() - q.front() + 1) > m) q.pop_front();
+		if (i >= m) cout << a[q.front()] << endl;
+	}
+
+	return 0;
+}
